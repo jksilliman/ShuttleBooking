09d1bc461facb3378463023d305efda85524571f
Jesse Silliman <jksilliman@gmail.com>
2012-02-18 13:15:59 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShuttleBooking::Application.config.secret_token = '3916e26936032e82c7b260c18219215d3001131f5148ef7f3dc0d95dcc2891569773e908f7a91864a3df16d5fd4bcc225767d1a0b6a5d40a8a131af6c84572d0'
